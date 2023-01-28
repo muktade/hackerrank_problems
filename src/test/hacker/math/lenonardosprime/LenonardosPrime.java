53e2bf71f553852f742c20a02ab46a0a681fbdef
muktadeer <muktadeer@gmail.com>
2023-01-28 21:39:10 +0600

    leonardos prime ok

--- src/test/hacker/math/lenonardosprime/LenonardosPrime.java
+++ src/test/hacker/math/lenonardosprime/LenonardosPrime.java
@@ -1,4 +1,40 @@
 package test.hacker.math.lenonardosprime;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class LenonardosPrime {
+    public static int primeCount(long n) {
+        // Write your code here
+        int[] ar = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59};
+        int cont = 0;
+        for (int i = 0; i < ar.length; i++) {
+            if(n>=ar[i]){
+                n = n/ar[i];
+                cont++;
+            }else {
+                break;
+            }
+
+//            p =p * ar[i];
+//            if (p > 0) {
+//                if (p > n) {
+//                    return i;
+//                } else if (n > p) {
+//                    cont = cont + 1;
+//                }
+//            }
+        }
+            return cont;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i<=n;i++){
+
+            System.out.println(primeCount(sc.nextLong()));
+        }
+        sc.close();
+    }
 }
